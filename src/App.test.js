e768a427bf9607c5243baebd08419b5f32717b3c
Leon Talbert <leon.talbert@gmail.com>
2021-09-15 21:19:34 +0800

    responsive styled components

--- src/App.test.js
+++ src/App.test.js
@@ -4,4 +4,5 @@ import App from './App';
 describe('App', () => {
   it.todo('should redirect to last completed step when user lands')
   it.todo('should save last completed step to local storage')
+  it.todo('should render correct styles depending on breakpoint')
 })
---
+++ src/utils/styledComponents.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { css } from 'styled-components';
+
+export const sizes = {
+    maxWidth: '1226',
+    tablet: '980',
+    mobile: '768',
+    sMobile: '480',
+};
+
+// iterate through the sizes and create a media template
+export const smallerThan = Object.keys(sizes).reduce((accumulator, label) => {
+    // use em in breakpoints to work properly cross-browser and support users
+    // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
+    const emSize = sizes[label] / 16;
+    const newAcc = accumulator;
+    newAcc[label] = (...args) => css`
+  @media screen and (max-width: ${emSize}em) {
+      ${css(...args)}
+    }
+  `;
+
+    return newAcc;
+}, {});
+
+export const largerThan = Object.keys(sizes).reduce((accumulator, label) => {
+    // use em in breakpoints to work properly cross-browser and support users
+    // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
+    const emSize = sizes[label] / 16;
+    const newAcc = accumulator;
+    newAcc[label] = (...args) => css`
+  @media screen and (min-width: ${emSize}em) {
+      ${css(...args)}
+    }
+  `;
+
+    return newAcc;
+}, {});
+
+export const smallerThanRes = (res) => {
+    const emSize = res / 16;
+    return (...args) => css`
+    @media screen and (max-width: ${emSize}em) {
+      ${css(...args)}
+    }
+  `;
+};
