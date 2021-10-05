0531b6dc818dcb50c5342e82e35e29cd7b393b16
Leon Talbert <leon.talbert@gmail.com>
2021-10-05 15:35:16 +0800

    SAVEPOINT

--- src/pages/ENSConstitution/ENSConstitutionSummary.js
+++ src/pages/ENSConstitution/ENSConstitutionSummary.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import styled from "styled-components";
+
+import {ContentBoxWithHeader, NarrowColumn} from "../../components/layout";
+import SectionHeader from "./SectionHeader";
+import Footer from "../../components/Footer";
+import {getConstitution} from "./constitutionHelpers";
+
+const ContentContainer = styled.div`
+    display: grid;
+    gap: 20px;
+`
 
 const EnsConstitutionSummary = () => {
+    const constitution = getConstitution();
+
     return (
-        <div>
-            ENS Constitution summary
-        </div>
+        <NarrowColumn>
+           Constitution summary
+        </NarrowColumn>
     );
 };
 
