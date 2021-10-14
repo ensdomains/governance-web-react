cc75fae6d0819f6a209766808f455c22e7908b51
Leon Talbert <leon.talbert@gmail.com>
2021-10-14 15:48:26 +0800

    update inelgible claim page

--- src/pages/ENSConstitution/ENSConstitutionVoting.js
+++ src/pages/ENSConstitution/ENSConstitutionVoting.js
@@ -1,4 +1,4 @@
-import React, {useState, useCallback, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import {useHistory} from "react-router-dom";
 
