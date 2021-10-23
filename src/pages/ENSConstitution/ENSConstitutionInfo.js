072596ce1279ed363eb8964ebf39404637e67ebc
Leon Talbert <leon.talbert@gmail.com>
2021-10-23 13:22:37 +0800

    constitution styling updates

--- src/pages/ENSConstitution/ENSConstitutionInfo.js
+++ src/pages/ENSConstitution/ENSConstitutionInfo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import SectionHeader from "./SectionHeader";
 import Footer from '../../components/Footer'
@@ -7,6 +8,9 @@ import {ContentBoxWithHeader, NarrowColumn} from "../../components/layout";
 import Gap from "../../components/Gap";
 import Token from "../../components/Token";
 
+const BlackBold = styled.b`
+  color: black;
+`
 
 const ENSConstitutionInfo = ({handleBack, handleNext, currentStep, setCurrentStep}) => {
     return (
@@ -18,13 +22,17 @@ const ENSConstitutionInfo = ({handleBack, handleNext, currentStep, setCurrentSte
                 <Gap height={3}/>
                 <Content>
                     We have created an initial set of community guidelines for which you may vote for or against with your allocation of
-                    claimable <Token />. Voting is optional, are conducted off-chain using Snapshot.
+                    claimable <Token />. Voting is optional, are conducted off-chain using <BlackBold>Snapshot</BlackBold>.
                 </Content>
-
                 <Gap height={3}/>
                 <Content>
                     We hope these rules form the basis for a strong community,
                     and recognize the participation of the contributors who have made ENS what it is today.
+                </Content>
+                <Gap height={3}/>
+                <Content>
+                    A minimum consensus of 75% of votes is required to
+                    ratify a given Article of the Constitution.
                 </Content>
                 <Gap height={3}/>
             </ContentBoxWithHeader>
