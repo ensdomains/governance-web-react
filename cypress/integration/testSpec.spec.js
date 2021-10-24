8a11a44becb339cecbb9ed05d348824abd7b9ca8
Leon Talbert <leon.talbert@gmail.com>
2021-10-24 20:09:35 +0800

    initial attempt

---
+++ cypress/integration/testSpec.spec.js
@@ -0,0 +1,10 @@
+describe('My First Test', () => {
+    before(() => {
+        cy.setupMetamask();
+        cy.changeMetamaskNetwork('tenderly')
+        cy.visit('/')
+    });
+    it('Does not do much!', () => {
+        cy.visit('http://localhost:3000')
+    })
+})
