bccf26d41220c08b7f2e80a833789e3bbf7f9cc8
Jeff Lau <jeff-lau@live.com>
2021-11-05 17:05:05 +0800

    Add reverse records filter, delegate referral ranking

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,6 +1,7 @@
 export const PROPOSAL_ID = "QmQ4m6hVGawbo2cjVVUQx2Noz4mXJekMYxDrmg4ZN5GCXq";
 export const SPACE_ID = "bananana.eth";
-export const ALLOCATION_ENDPOINT = "https://us-central1-ens-manager.cloudfunctions.net/getvotes";
+export const ALLOCATION_ENDPOINT =
+  "https://us-central1-ens-manager.cloudfunctions.net/getvotes";
 
 export const getEnv = () =>
   window.location.href.includes("localhost") ? "dev" : "prod";
@@ -8,14 +9,17 @@ export const getEnv = () =>
 export const isDev = () => getEnv() === "dev";
 
 export const getENSTokenContractAddress = () =>
-  isDev()
-    ? "token.ensdao.eth"
-    : "token.ensdao.eth";
+  isDev() ? "token.ensdao.eth" : "token.ensdao.eth";
 
 export const getENSDelegateContractAddress = () =>
   isDev()
     ? "0x9c9Be865067d9acC5da6b73Fd48EAa3B6c382858"
     : "0x9c9Be865067d9acC5da6b73Fd48EAa3B6c382858";
+
+export const getReverseRecordsAddress = () =>
+  isDev()
+    ? "0x3671aE578E63FdF66ad4F3E12CC0c0d71Ac7510C"
+    : "0x3671aE578E63FdF66ad4F3E12CC0c0d71Ac7510C";
 
 export const generateMerkleShardUrl = (address) =>
   `/airdrops/mainnet/${address?.slice(2, 4)}.json`;
