b4368a0663238d96458e723b6400aa20f07bcacf
Leon Talbert <leon.talbert@gmail.com>
2021-11-06 19:05:13 +0800

    header tests

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,3 +1,38 @@
+import { render, screen } from '@testing-library/react'
+import { useQuery } from '@apollo/client'
+import { StaticRouter } from 'react-router-dom'
+
+import Header from './Header'
+
+jest.mock('@apollo/client', () => ({
+  __esModule: true,
+  ...jest.requireActual('@apollo/client'),
+  useQuery: jest.fn()
+}))
+
 describe("Header", () => {
-  it.todo("should render the right copy on each step");
+  it('should render Proflie when connected', () => {
+    useQuery.mockImplementation(() => ({ data: { isConnected: true, address: '0xaddress' } }))
+
+    const context = {}
+    render(
+        <StaticRouter location={'/'} context={context}>
+          <Header />
+        </StaticRouter>
+    )
+
+    expect(screen.getByText('0xaddress')).toBeTruthy()
+  })
+  it('should render connect button when not connected', () => {
+    useQuery.mockImplementation(() => ({ data: { isConnected: false, address: '0xaddress' } }))
+
+    const context = {}
+    render(
+        <StaticRouter location={'/'} context={context}>
+          <Header />
+        </StaticRouter>
+    )
+
+    expect(screen.getByText('Connect')).toBeTruthy()
+  })
 });
